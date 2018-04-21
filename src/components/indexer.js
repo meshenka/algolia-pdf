b0e22fd957ab938bd31ca3a1ab550646677a50b3
Sylvain Gogel <sylvain.gogel@gmail.com>
2018-04-21 22:13:18 +0200

    move index name to configuration

--- src/components/indexer.js
+++ src/components/indexer.js
@@ -4,7 +4,7 @@ import algoliasearch from 'algoliasearch'
 const indexDocuments = (documents) => {
 
   const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY)
-  const index = client.initIndex('documents')
+  const index = client.initIndex(process.env.ALGOLIA_INDEX)
 
   return new Promise((resolve, reject) => {
     index.addObjects(documents, (err, content) => {
